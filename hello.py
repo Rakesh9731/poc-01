0cbb16c54e80b472dced0d7d8e3546332a48adb6
Ubuntu <ubuntu@ip-172-31-16-126.ec2.internal>
2025-08-21 12:20:58 +0000

    application modifieed

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 def get_greeting():
-    return "hello there welcome"
+    return "hello rockie, welcome to ur application"
 
 @app.route("/")
 def home():
