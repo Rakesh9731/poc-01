100dcc0b244c8bb0c8d107d28dab6b21045716ca
Ubuntu <ubuntu@ip-172-31-16-126.ec2.internal>
2025-08-21 09:10:35 +0000

    feat: Add Dockerfile and Flask web application

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 def get_greeting():
     return "hello there welcome"
 
+@app.route("/")
+def home():
+    return get_greeting()
+
 if __name__ == "__main__":
-    print(get_greeting())
+    app.run(host="0.0.0.0", port=9000)
