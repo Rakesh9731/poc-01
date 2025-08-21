7bc2ed97b6edc8a329c4fbbfaffb9fef913d824f
Ubuntu <ubuntu@ip-172-31-16-126.ec2.internal>
2025-08-21 12:25:38 +0000

    application modifieed

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,4 +3,4 @@ sys.path.append("/home/ubuntu/gemini/poc-01")
 from hello import get_greeting
 
 def test_get_greeting():
-    assert get_greeting() == "hello there welcome"
+    assert get_greeting() == "hello rockie, welcome to ur application"
