746302944f7964a50e122885dd8fd25754256142
Ubuntu <ubuntu@ip-172-31-16-126.ec2.internal>
2025-08-21 08:54:00 +0000

    Add hello world program and test

---
+++ hello.py
@@ -0,0 +1,5 @@
+def get_greeting():
+    return "hello there welcome"
+
+if __name__ == "__main__":
+    print(get_greeting())
---
+++ tests/test_hello.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append("/home/ubuntu/gemini/poc-01")
+from hello import get_greeting
+
+def test_get_greeting():
+    assert get_greeting() == "hello there welcome"
